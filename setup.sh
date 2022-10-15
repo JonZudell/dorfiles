1085c81704705a93ca8e704f02998e3d695d67ff
Jon Zudell <jonathon.zudell@gmail.com>
2022-10-15 23:52:26 +0000

    half-man half-madness

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 apt-get update && apt-get -y install build-essential procps curl file git
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+/home/linuxbrew/.linuxbrew/bin/brew shellenv >> ~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv >> ~/.profile)"
+
+brew bundle --file ~/dotfiles/Brewfile
