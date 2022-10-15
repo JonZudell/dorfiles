e99470495af3fc668da35d7d148c356d5f5bccad
Jonathon Zudell <jonathon.zudell@gmail.com>
2022-10-15 18:22:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update && apt-get -y install build-essential procps curl file git
-
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
