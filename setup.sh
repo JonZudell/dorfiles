56e097252ab1cdfd9bb1d94de644de6bb824abe7
Jon Zudell <jonathon.zudell@gmail.com>
2022-10-16 16:22:52 +0000

    updated

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
 
 apt-get update && apt-get -y install build-essential procps curl file git
+cp ./.gitignore ~/.gitignore
+
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 /home/linuxbrew/.linuxbrew/bin/brew shellenv >> ~/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv >> ~/.profile)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew bundle --file ~/dotfiles/Brewfile
+brew bundle --file ./Brewfile
